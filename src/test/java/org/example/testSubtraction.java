373c46baa4322a4b9733518cdf1d8cbf1aec0080
1001234567 <17804532071@163.com>
2024-09-23 13:53:18 +0800

    Fix subtract method to pass testSubtraction

--- src/test/java/org/example/testSubtraction.java
+++ src/test/java/org/example/testSubtraction.java
@@ -1,4 +1,13 @@
-@Test
-public int subtract(int a, int b) {
-    return a - b;
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testSubtraction {
+
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2), "4 - 2 应该等于 2");
+    }
 }
